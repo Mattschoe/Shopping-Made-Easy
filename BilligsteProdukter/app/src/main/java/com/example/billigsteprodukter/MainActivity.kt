ac26cf847161ba3e16260d69f4e45320f3329bb1
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2025-04-06 20:48:27 +0200

    ARGH

--- BilligsteProdukter/app/src/main/java/com/example/billigsteprodukter/MainActivity.kt
+++ BilligsteProdukter/app/src/main/java/com/example/billigsteprodukter/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.billigsteprodukter
 
 import android.os.Bundle
+import android.view.SurfaceView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.billigsteprodukter.ui.theme.BilligsteProdukterTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,17 +21,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent { //Defines layout with the 'Composable' functions.
             BilligsteProdukterTheme {
-                Survey(true)
+                val questions = listOf("Mælk?", "Æg?", "Ost?", "Tis?")
+                questionList(questions)
             }
         }
     }
 }
 
 @Composable
-fun Survey(buttonClicked: Boolean) { //Enhver composable function skal have en modifier parameter
-    Column {
-        RadioButton(buttonClicked, onClick = {
-            println("Test")
-        })
+fun questionList(questions: List<String>) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Column {
+            for (question in questions) {
+                surveyAnswer(question);
+            }
+        }
     }
+}
+
+@Composable
+fun surveyAnswer(question: String) {
+    Text(question, padding(24.dp))
 }

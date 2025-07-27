d93ca86571ccc5e00d88951738860a094fa96c4d
Matthias Nielsen <matthias.s.nielsen@gmail.com>
2025-07-27 21:13:17 +0200

    Added en general FuzzyMatcher og refactoriserede ParserFactory med den i mind

--- app/src/main/java/weberstudio/app/billigsteprodukter/logic/parsers/ParserFactory.kt
+++ app/src/main/java/weberstudio/app/billigsteprodukter/logic/parsers/ParserFactory.kt
@@ -1,21 +1,25 @@
 package weberstudio.app.billigsteprodukter.logic.parsers
 
 import com.google.mlkit.vision.text.Text
+import weberstudio.app.billigsteprodukter.logic.FuzzyMatcher
 import weberstudio.app.billigsteprodukter.ui.ParsingState
 
 ///Parses the given textblock into the correct store
 object ParserFactory {
+    val fuzzyMatcher = FuzzyMatcher()
+
     ///Runs through the entire receipt and finds the correct Store
     fun parseReceipt(receipt: Text): StoreParser? {
         for (block in receipt.textBlocks) {
             for (line in block.lines) {
                 val lineText = normalizeText(line.text)
-                if (lineText.contains("ETTO")) return NettoParser
-                else if (lineText == "REMA") return RemaParser
-                else if (lineText == "COOP") return CoopParser
-                else if (lineText == "MENU") return MenuParser
-                else if (lineText == "LIDL") return LidlParser
-                else if (lineText == "SUPERBRUGSEN") return SuperBrugsenParser
+
+                if (fuzzyMatcher.match("NETTO", listOf("NETTO"), 0.85f, 0.3f)) return NettoParser
+                else if (fuzzyMatcher.match("REMA", listOf("REMA"), 0.85f, 0.3f)) return RemaParser
+                else if (fuzzyMatcher.match("COOP", listOf("COOP"), 0.85f, 0.3f)) return CoopParser
+                else if (fuzzyMatcher.match("MENU", listOf("MENU"), 0.85f, 0.3f)) return MenuParser
+                else if (fuzzyMatcher.match("LIDL", listOf("LIDL"), 0.85f, 0.3f)) return LidlParser
+                else if (fuzzyMatcher.match("SUPERBRUGSEN", listOf("SUPERBRUGSEN"), 0.85f, 0.3f)) return SuperBrugsenParser
             }
         }
         return null

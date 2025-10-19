d3462bdb571940de8a1ef6f53667f92b9bfabf52
Matthias Nielsen <mscn@itu.dk>
2025-10-19 17:30:34 +0200

    chore: remove old file

--- app/src/main/java/weberstudio/app/billigsteprodukter/ui/components/StoreSection.kt
+++
@@ -1,6 +0,0 @@
-package weberstudio.app.billigsteprodukter.ui.components
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import weberstudio.app.billigsteprodukter.logic.Store
-

d68e69a3327668e7420c08c4414186aef39ce39b
Matthias Nielsen <mscn@itu.dk>
2025-11-11 14:31:27 +0100

    Revert "fix: No error when chosing product_total"
    This reverts commit a86f7f7c3efc43bd8574d7b7c8ff543797963bc8.

--- app/src/main/java/weberstudio/app/billigsteprodukter/ui/pages/receiptScanning/ReceiptViewModel.kt
+++ app/src/main/java/weberstudio/app/billigsteprodukter/ui/pages/receiptScanning/ReceiptViewModel.kt
@@ -3,7 +3,6 @@ package weberstudio.app.billigsteprodukter.ui.pages.receiptScanning
 import android.app.Application
 import android.content.Context
 import android.net.Uri
-import weberstudio.app.billigsteprodukter.logic.Formatter.isIshEqualTo
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
@@ -263,19 +262,7 @@ class ReceiptViewModel(application: Application): AndroidViewModel(application) 
                 val receiptID = receiptRepo.addReceiptProducts(receipt, parsedText.products)
                 Logger.log(tag, "Receipt saved with ID: $receiptID, products: ${parsedText.products.size}")
 
-                //Removes the total error if product total corresponds with what the parser calculated
-                val errors = if (_totalOption.value == PRODUCT_TOTAL) {
-                    val isValid = parsedText.products
-                        .sumOf { it.price.toDouble() }
-                        .toFloat()
-                        .isIshEqualTo(parsedText.total)
-                    if (isValid) parsedText.scanErrors.copy(totalError = false)
-                    else parsedText.scanErrors
-                } else {
-                    parsedText.scanErrors
-                }
-
-                cameraCoordinator.setScanValidation(receiptID, errors)
+                cameraCoordinator.setScanValidation(receiptID, parsedText.scanErrors)
                 app.activityLogger.logReceiptScan(receipt.copy(receiptID = receiptID))
 
                 _parsingState.value = ParsingState.Success(store, receiptID)

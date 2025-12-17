15ea9c0364f9c136871a8071916353234832077b
Matthias Nielsen <mscn@itu.dk>
2025-12-17 09:10:22 +0100

    chore: change version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "weberstudio.app.billigsteprodukter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "3.1"
+        versionCode = 5
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
